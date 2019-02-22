36cdf48d05b7b0d870780dc1680bb56b5f02a773
Stephen Chu <stephen.chu530@gmail.com>
2019-02-22 15:43:49 -0800

    fix: changed logic for checking names and ids

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -2,10 +2,16 @@ $(document).ready(function () {
   $(".amenities UL LI INPUT").css("margin-right", "10px");
   let id_dict = {}
   $('input[type="checkbox"]').click(function () {
+
     if ($(this).is(":checked")) {
-      id_dict[$(this).attr('data-id')] = 1;
+      id_dict[$(this).attr('data-id')] = $(this).attr('data-name');
     } else if ($(this).is(":not(:checked)")) {
       delete id_dict[$(this).attr('data-id')];
     }
+    let alist = [];
+    for (let k in id_dict) {
+      alist.push(id_dict[k]);
+    }
+    $('.amenities h4').text(alist.join(", "));
   });
 });
