3b232d63c4439014bb15735d15ca857b238c3c0a
hcheung01 <hcheung01@gmail.com>
2019-02-23 02:02:57 +0000

    feat: added places_search function

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  $('.amenities UL LI INPUT').css('margin-right', '10px');
+  let idDict = {};
+  $('input[type="checkbox"]').click(function () {
+    if ($(this).is(':checked')) {
+      idDict[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else if ($(this).is(':not(:checked)')) {
+      delete idDict[$(this).attr('data-id')];
+    }
+    let alist = [];
+    for (let k in idDict) {
+      alist.push(idDict[k]);
+    }
+    $('.amenities h4').text(alist.join(', '));
+  });
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').toggleClass('available');
+    }
+  });
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    type: 'POST',
+    dataType: 'json',
+    contentType: 'application/json',
+    data: JSON.stringify({}),
+    success: function(data) {
+      $.each(data, function(k, v) {
+	
+
+      });
+    }
+  });
+});
